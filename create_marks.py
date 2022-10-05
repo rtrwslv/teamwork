4a67751365c5289fd28d64586d24557f92588f24
rtrwslv <yarsl.filatov@gmail.com>
2022-10-05 14:39:41 +0300

    Сделал исключения на пересоздание файла

--- create_marks.py
+++ create_marks.py
@@ -1,8 +1,11 @@
 import random
 file = open("names", "r")
-file2 = open("names and marks", "w")
-bottom = 2
-top = 5
-mass = file.readlines()
-for i in mass:
-    file2.write(i.replace("\n", "") + ": " + str(random.randint(bottom, top)) + "\n")
+try:
+    open("names and marks", "r")
+except FileNotFoundError:
+    file2 = open("names and marks", "w")
+    bottom = 2
+    top = 5
+    mass = file.readlines()
+    for i in mass:
+        file2.write(i.replace("\n", "") + ": " + str(random.randint(bottom, top)) + "\n")
