ba1c34bdaab16f3623957562a3d18521b07747ce
rtrwslv <yarsl.filatov@gmail.com>
2022-10-05 14:32:09 +0300

    Поменял границы на константы

--- create_marks.py
+++ create_marks.py
@@ -1,6 +1,8 @@
 import random
 file = open("names", "r")
 file2 = open("names and marks", "w")
+bottom = 2
+top = 5
 mass = file.readlines()
 for i in mass:
-    file2.write(i.replace("\n", "") + ": " + str(random.randint(2, 5)) + "\n")
+    file2.write(i.replace("\n", "") + ": " + str(random.randint(bottom, top)) + "\n")
