425ea2febdfd7d655652a9a1c57bcc951f3b1b2e
nester256 <nestvlad2004@gmail.com>
2022-10-25 11:45:06 +0300

    Add new test to calculate the correctness of the algorithm

--- test_create_marks.py
+++ test_create_marks.py
@@ -3,3 +3,14 @@ import os
 
 def test_create_marks():
     assert os.path.exists("names_and_marks") is True
+
+
+def test_average_value_calculation():
+    file = open("names_and_marks_for_test", "r")
+    all_marks = 0
+    counter = 0
+    for line in file:
+        mark = int(line.split(":")[-1])
+        all_marks += mark
+        counter += 1
+    assert float(all_marks / counter) == 3.3333333333333335
